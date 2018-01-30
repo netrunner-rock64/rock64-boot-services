170623a989233a504ab8f44a800c803c14dd2bff
Leszek Lesner <leszek.lesner@web.de>
2018-01-30 13:53:26 +0100

    Adapt for rock64

---
+++ usr/sbin/rock64_init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Console unblack
+echo 0 > /sys/class/graphics/fb0/blank
+
+
+if [ -n "$timestamp" ]; then
+DT=`date +%Y%m%d`
+	if [ "$DT" -lt "$timestamp" ]; then
+		date -s "$timestamp"
+  fi
+fi
+
+timedatectl set-ntp 1
